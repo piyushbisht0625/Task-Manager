3da2e3f2b05bfdb9bba1a6eeed56647383449a05
Piyush Bisht <piyushbisht0625@gmail.com>
2024-04-25 20:03:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const addNewCard = () => {
   const myCard = {
     id: `${Date.now()}`, //  it always gives the unique value    
 
-     
+      
     image: document.getElementById("imgurl").value, 
     TaskTitle: document.getElementById("taskTitle").value,
     TaskType: document.getElementById("taskType").value,
